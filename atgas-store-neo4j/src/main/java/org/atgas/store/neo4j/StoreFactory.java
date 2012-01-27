3b45913231f18c79633da1b43da4589697b17249
Kenneth Leider <kennethleider@gmail.com>
2012-01-26 22:48:37 -0700

    Basic functionality exist

--- atgas-store-neo4j/src/main/java/org/atgas/store/neo4j/StoreFactory.java
+++ atgas-store-neo4j/src/main/java/org/atgas/store/neo4j/StoreFactory.java
@@ -1,15 +1,48 @@
 package org.atgas.store.neo4j;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.atgas.store.BuiltIns;
 import org.atgas.store.Store;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 
 public class StoreFactory {
+    private static final String DB_PATH = "repository/neo4j";
+    private static final Map<String, String> OPTIONS = Collections.singletonMap("allow_store_upgrade","true");
+    String greeting;
+    GraphDatabaseService graphDb;
+    Node firstNode;
+    Node secondNode;
+    Relationship relationship;
 
-	public Store create() {
-		EmbeddedGraphDatabase database = new EmbeddedGraphDatabase("repository/neo4j");
-		StoreImplementation retval = new StoreImplementation(database);
-		retval.addThings(BuiltIns.getBuiltInThings());
-		return retval;
-	}
+    public static void main(final String[] args) {
+        new StoreFactory().create();
+    }
+    private static void registerShutdownHook( final GraphDatabaseService graphDb )
+    {
+        // Registers a shutdown hook for the Neo4j instance so that it
+        // shuts down nicely when the VM exits (even if you "Ctrl-C" the
+        // running example before it's completed)
+        Runtime.getRuntime().addShutdownHook( new Thread()
+        {
+            @Override
+            public void run()
+            {
+                graphDb.shutdown();
+            }
+        } );
+    }
+
+    public Store create() {
+        EmbeddedGraphDatabase database = new EmbeddedGraphDatabase( DB_PATH, OPTIONS);
+        registerShutdownHook( database );
+
+        StoreImplementation retval = new StoreImplementation(database);
+        retval.addThings(BuiltIns.getBuiltInThings());
+        return retval;
+    }
 }
--- atgas-store-neo4j/src/main/java/org/atgas/store/neo4j/StoreImplementation.java
+++ atgas-store-neo4j/src/main/java/org/atgas/store/neo4j/StoreImplementation.java
@@ -12,101 +12,97 @@ import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.index.Index;
 
 class StoreImplementation implements Store {
-	private final GraphDatabaseService database;
 
-	public StoreImplementation(GraphDatabaseService database) {
-		this.database = database;
-	}
-	
-	@Override
-	public void addThings(Thing... things) {
-		Transaction tx = database.beginTx();
-		try {
-			Index<Node> index = database.index().forNodes("atgasID");
-			for (Thing thing : things) {
-				
-				Node node = findByID(thing.sourceID);
-				// The id, source and standard cannot change once set.
-				if (node == null) {
-					node = database.createNode();
-					node.setProperty("atgas", thing.getId());
-					index.add(node, "id", thing.getId());
-					index.add(node, "source", thing.sourceID);
-					index.add(node, "standard", thing.standardID);
-					Node source = findByID(thing.sourceID);
-					if (source != null) {
-						node.createRelationshipTo(source, AtgasRelationshipType.Source);
-					}
-					
-					Node standard = findByID(thing.standardID);
-					if (standard != null) {
-						node.createRelationshipTo(standard, AtgasRelationshipType.Standard);
-					}
-				}
-				
-				// Always update the properties
-				for (Entry<String, String> property : thing.getPropertyValues()) {
-					node.setProperty(property.getKey(), property.getValue());
-				}
-				
-			}
-			tx.success();
-		} finally {
-			tx.finish();
-		}
-		// TODO Auto-generated method stub
-		
-	}
+    private final GraphDatabaseService database;
 
-	private Node findByID(String sourceID) {
-		Index<Node> index = database.index().forNodes("atgas");
-		return index.get("id", sourceID).getSingle();
-	}
+    StoreImplementation(GraphDatabaseService database) {
+        this.database = database;
+    }
 
-	@Override
-	public void removeThings(Thing... things) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void addThings(Thing... things) {
+        Transaction tx = database.beginTx();
+        try {
+            Index<Node> index = database.index().forNodes("atgas");
+            for (Thing thing : things) {
 
-	@Override
-	public void addRelationships(Relationship... relationships) {
-		// TODO Auto-generated method stub
-		
-	}
+                Node node = findByID(thing.getId());
+                // The id, source and standard cannot change once set.
+                if (node == null) {
+                    node = database.createNode();
+                    String id = thing.getId();
+                    node.setProperty("id", thing.getId());
+                    index.add(node, "id", thing.getId());
+                    index.add(node, "source", thing.sourceID);
+                    index.add(node, "standard", thing.standardID);
+                    Node source = findByID(thing.sourceID);
+                    if (source != null) {
+                        node.createRelationshipTo(source, AtgasRelationshipType.Source);
+                    }
 
-	@Override
-	public void removeRelationships(Relationship... relationships) {
-		// TODO Auto-generated method stub
-		
-	}
+                    Node standard = findByID(thing.standardID);
+                    if (standard != null) {
+                        node.createRelationshipTo(standard, AtgasRelationshipType.Standard);
+                    }
+                }
 
-	@Override
-	public Set<Thing> query(String query) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+                // Always update the properties
+                for (Entry<String, String> property : thing.getPropertyValues()) {
+                    node.setProperty(property.getKey(), property.getValue());
+                }
 
-	@Override
-	public Set<Thing> query(String query, String queryLanguage) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+            }
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+    }
 
-	@Override
-	public Set<String> supportedQueryLanguages() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public void close() {
-		database.shutdown();
-	}
-	
-	@Override
-	protected void finalize() throws Throwable {
-		close();   
-	}
+    private Node findByID(String sourceID) {
+        Index<Node> index = database.index().forNodes("atgas");
+        return index.get("id", sourceID).getSingle();
+    }
 
+    @Override
+    public void removeThings(Thing... things) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void addRelationships(Relationship... relationships) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void removeRelationships(Relationship... relationships) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public Set<Thing> query(String query) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<Thing> query(String query, String queryLanguage) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<String> supportedQueryLanguages() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void close() {
+        database.shutdown();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        close();
+    }
 }
