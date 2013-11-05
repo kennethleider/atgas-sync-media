0f3fbf27694f505ca374412824098eed2a53f118
Ken Leider <ken.leider@canoeventures.com>
2013-11-05 15:11:40 -0700

    Starting to get the conversion stuff coded.

--- mythtv/src/setup.groovy
+++ mythtv/src/setup.groovy
@@ -1,38 +1,34 @@
-#!/usr/bin/groovy -d
-
-@Grab('mysql:mysql-connector-java:5.1.26')
-@GrabConfig(systemClassLoader=true)
-import com.mysql.jdbc.Driver 
-import groovy.sql.Sql
+#!/usr/bin/groovy
 
 scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent
-config = new ConfigSlurper().parse(new File("${scriptDir}/transcode.cfg").toURI());
-
-sql = Sql.newInstance(config.db.url, config.db.username, config.db.password)
+config = new ConfigSlurper().parse(new File("${scriptDir}/transcode.cfg").toURI().toURL());
 
 for (arg in args) {
     source = new File(arg) 
-    basename = (source.name =~ /(.*?)\..*/)[0][1]
-    workingDir = new File(config.work.root, basename)
-    configFile = new File(workingDir, "mythtv.cfg")
+
     if (!source.exists()) {
-        System.err.println("${source} does not exist.  SKIPPING")    
-        conintue;
+        System.err.println("${source} does not exist.  SKIPPING")
+        continue;
     }
-    
+
+    basename = (source.name =~ /(.*?)\..*/)[0][1]
+
+    workingDir = new File(config.work.root, basename)
+
+    configFile = new File(workingDir, "mythtv.cfg")
     if (!workingDir.exists() && !workingDir.mkdirs()) {
         System.err.println("Unabled to create ${workingDir}")    
-        conintue;
+        continue;
     }
     
     if (configFile.exists()) {
         println "${configFile} already exists"
     } else {
         println "Creating ${configFile}"
-        row = sql.firstRow("select title, subtitle, season, episode, seriesid, programid from recorded where basename = ${source.name}")
 
         mythConfig = new ConfigObject()
-        mythConfig.putAll(row)
+        mythConfig.put("source", source.absolutePath)
+        mythConfig.put("workingDir", workingDir.absolutePath)
         configFile.withWriter { mythConfig.writeTo(it) };
     }
 
